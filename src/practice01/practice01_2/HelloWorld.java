b88faa318eba2b32fc4f58e569975f5ba9938130
Lucas Toscaro <lucas@toscaro.com.br>
2021-04-01 13:43:15 -0300

    Organizing practice 01_2 name

--- src/practice01/practice01_2/HelloWorld.java
+++ src/practice01/practice01_2/HelloWorld.java
@@ -1,6 +1,6 @@
 package practice01.practice01_2;
 
-public class Practice01_2HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(args[0] + args[1] + args[2]);
     }
