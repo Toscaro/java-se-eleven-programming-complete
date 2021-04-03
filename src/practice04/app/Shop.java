c904918fb34db4a203b25328b46fd7648de310e9
Lucas Toscaro <lucas@toscaro.com.br>
2021-04-03 11:20:14 -0300

    Completing practice 04_4 working with javadoc and generating HTML doc.

--- src/practice04/app/Shop.java
+++ src/practice04/app/Shop.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package practice04.app;
 
 import practice04.data.Product;
@@ -5,15 +13,18 @@ import practice04.data.Product;
 import java.math.BigDecimal;
 
 /**
+ * {@code Shop} class represents an application that manages Products.
+ *
  * @author Toscaro
+ * @version 4.0
  */
 public class Shop {
 
     public static void main(String[] args) {
         //No constructor.
         Product p1 = new Product();
-//        p1.setId(10);
-//        p1.setName("Tea");
+        p1.setId(10);
+        p1.setName("Tea");
         p1.setPrice(BigDecimal.valueOf(1.99));
 
         System.out.println(p1);
--- src/practice04/data/Product.java
+++ src/practice04/data/Product.java
@@ -1,13 +1,35 @@
+/*
+ * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package practice04.data;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.NumberFormat;
 
 /**
+ * {@code Product} class represents properties and behaviours of
+ * product objects in the Product Management System.
+ * <br>
+ * Each product has an id, name, and price.
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ *
  * @author Toscaro
+ * @version 4.0
  */
 public class Product {
+    /**
+     * A constant that defines a
+     * {@link BigDecimal BigDecimal} value of the discount rate
+     * <br>
+     * Discount rate is 10%
+     */
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
 
     private int id;
@@ -39,6 +61,12 @@ public class Product {
         this.price = price;
     }
 
+    /**
+     * Calculates discount based on a product price and
+     * {@link DISCOUNT_RATE discount rate}
+     *
+     * @return a {@link BigDecimal BigDecimal} value of the discount
+     */
     public BigDecimal getDiscount() {
         BigDecimal discount = price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
         return discount;
